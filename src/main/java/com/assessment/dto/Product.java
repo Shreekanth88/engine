b4a788db66dc8996830e3acefe4fba03b54f742e
Shreekanth88 <shreekanthg.g@gmail.com>
2023-02-03 15:29:51 +0000

    feat: business rule engine enums & List of operations to return

---
+++ src/main/java/com/assessment/dto/Product.java
@@ -0,0 +1,15 @@
+package com.assessment.dto;
+
+import java.util.List;
+
+public class Product {
+  public List<String> operations;
+
+  public List<String> getOperations() {
+    return operations;
+  }
+
+  public void setOperations(List<String> operations) {
+    this.operations = operations;
+  }
+}
---
+++ src/main/java/com/assessment/util/ProductTypes.java
@@ -0,0 +1,10 @@
+package com.assessment.util;
+
+public enum ProductTypes {
+  physical_product,
+  book,
+  membership,
+  upgrade,
+  video,
+  other
+}
